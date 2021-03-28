720879d9654e41d0918058a00a1b97bcbc80e9c1
You Yi <yyou@explainconsultancy.com>
2021-03-29 01:47:44 +0200

    change attribut name (cost_t, cost_p and cost_total), and fix an error in update travel time

--- iGraph_test.py
+++ iGraph_test.py
@@ -66,7 +66,7 @@ def readDemand():
     for x in inFile:
         tmpIn = [t.strip() for t in x.strip().split(",")]
         A, B = int(float(tmpIn[0])), int(float(tmpIn[1]))
-        # if A > 50:
+        # if A > 300:
         #     break
         tripSet[A, B] = initDemand(tmpIn)
         if A not in zoneSet:
@@ -130,7 +130,6 @@ def readNetwork(g):
             distance = round(float(tmpIn[indexDistance]),2)
             speed = int(float(tmpIn[indexSpeed]))
             peage = float(tmpIn[indexPeage])
-            ftt = 60 * distance / speed
             # calculate cost
             if linktype == 100:
                 cost_hp = 0.21 * max(distance - 0.3, 0) * math.pow(1 + TCAM1530/100, min(HORIZON, 2030)-2017) *\
@@ -138,15 +137,14 @@ def readNetwork(g):
             else:
                 cost_hp = 0.21 * distance * math.pow(1 + TCAM1530/100, min(HORIZON, 2030)-2017) *\
                     math.pow(1 + TCAM3050/100, max(HORIZON, 2030) - 2030)
-            peage = peage* distance * math.pow(1 - 0.5/100, HORIZON - 2017)
-            cost = int(4 * (cost_hp + peage) + ftt)
+            peage = peage * distance * math.pow(1 - 0.5/100, HORIZON - 2017)
             g.es[edge_id]['capacity'] = int(tmpIn[indexCapacity]) if int(tmpIn[indexCapacity]) != 0 else 1800
             g.es[edge_id]['linktype'] = linktype
             g.es[edge_id]['peage'] = peage
             g.es[edge_id]['distance'] = distance
-            g.es[edge_id]['t'] = round(ftt,2)
-            g.es[edge_id]['p'] = round(cost_hp + peage, 2)
-            g.es[edge_id]['cost'] = cost
+            g.es[edge_id]['cost_p'] = round(cost_hp + peage, 4)
+            g.es[edge_id]['cost_t'] = round(60 * distance / speed, 4)
+            g.es[edge_id]['cost_total'] = g.es[edge_id]['cost_t'] + 4 * g.es[edge_id]['cost_p']
             g.es[edge_id]['nature'] = str(tmpIn[indexNature])
             g.es[edge_id]['nb_voies'] = str(tmpIn[indexNbVoie])
             g.es[edge_id]['urbain'] = str(tmpIn[indexUrbain])
@@ -172,17 +170,15 @@ def readNetwork(g):
 #%%        
 def updateTravelTime():
     '''
-    This method updates the travel time ['t'] on the links with the current flow
+    This method updates the travel time ['cost_t'] on the links with the current flow
     '''
     gamma_v = 0.41
     alpha_v = 4
     chi_v = 2
-    e = 3
     for edge in g.es:
         if edge['class'] == 'Autoroute':
             if edge['urbain'] == 'Non':
                 gamma_v = 0.34
-                e = 2.5
                 if edge['nb_voies'] in [3,4]:
                     chi_v = 2.6
                     alpha_v = 6
@@ -192,7 +188,6 @@ def updateTravelTime():
             elif edge['urbain'] == 'Oui':
                 gamma_v = 0.41
                 chi_v = 2
-                e = 3
                 if edge['nb_voies'] in [3,4]:
                     alpha_v = 6
                 elif edge['nb_voies'] == 2:
@@ -200,7 +195,6 @@ def updateTravelTime():
         elif edge['class'] == 'Nationale':
             if edge['urbain'] == 'Non':
                 gamma_v = 0.41
-                e = 2.5
                 if edge['nature'] == 'Route a 2 chaussees':
                     chi_v = 2.6
                     alpha_v = 4
@@ -214,7 +208,6 @@ def updateTravelTime():
             # urbain
             elif edge['urbain'] == 'Oui':
                 chi_v = 1.6
-                e = 3
                 if edge['nature'] == 'Route a 1 chaussee' and edge['nb_voies'] == 1:
                     alpha_v = 3.4
                     gamma_v = 5.6
@@ -223,7 +216,6 @@ def updateTravelTime():
                     gamma_v = 2.7
             
         elif edge['class'] == 'Departementale':
-            e = 3
             if edge['urbain'] == 'Non':
                 gamma_v = 0.41
                 if edge['nature'] == 'Route a 2 chaussees':
@@ -266,8 +258,8 @@ def updateTravelTime():
                 elif edge['nb_voies'] == 2:
                     alpha_v = 3.5
                     gamma_v = 2.7
-        edge['t'] = edge['t'] * (1 + gamma_v * math.pow(((e+1)*edge['flow']/24*chi_v/edge['capacity']),alpha_v))
-        edge['cost'] = edge['t'] + edge['p']
+        edge['cost_t'] = edge['cost_t'] * (1 + gamma_v * math.pow((edge['flow']/24*chi_v/edge['capacity']),alpha_v))
+        edge['cost_total'] = edge['cost_t'] + 4 * edge['cost_p']
 
 
 def findAlpha(x_bar):
@@ -279,7 +271,7 @@ def findAlpha(x_bar):
     def df(alpha):
         sum_derivative = 0 ## this line is the derivative of the objective function.
         for i in range(len(g.es)):
-            sum_derivative = sum_derivative + (x_bar[i] - g.es[i]['flow'])*g.es[i]['cost']
+            sum_derivative = sum_derivative + (x_bar[i] - g.es[i]['flow'])*g.es[i]['cost_total']
         return sum_derivative
     sol2 = fsolve(df, np.array([0.1]))
     return max(0.1, min(1, sol2[0]))
@@ -304,14 +296,15 @@ def loadAON():
             except KeyError:
                 dem = 0.0
             try:
-                SPTT = SPTT + int(sum(g.es[paths[index]]['cost']) * dem)
+                SPTT = SPTT + int(sum(g.es[paths[index]]['cost_total']) * dem)
             except KeyError:
-                SPTT = 0.0
+                pass
             if r != s:
                 tripSet[(r, s)]['sptt'] = str(SPTT)
                 for edge in paths[index]:
                     x_bar[edge] = x_bar[edge] + dem
-        print("Origin: {}, Dijkstra time used: {} sedonds ".format(r, round(time.time() - timeDijkstra, 2)))
+        if int(r)%500 == 0:
+            print("Origin: {}, Dijkstra time used: {} sedonds ".format(r, round(time.time() - timeDijkstra, 2)))
     return SPTT, x_bar
 
 # def loadAON():
@@ -351,6 +344,7 @@ def assignment(algorithm, accuracy = 0.01, maxIter=100):
     x_bar = {l: 0.0 for l in range(len(g.es))}
     startP = time.time()
     while gap > accuracy:
+        startI  = time.time()
         print("iteration start :" + str(it))
         # Calculate the alpha
         if algorithm == "MSA" or it < 2:
@@ -360,11 +354,11 @@ def assignment(algorithm, accuracy = 0.01, maxIter=100):
         else:
             print("Terminating the program.....")
             print("The solution algorithm ", algorithm, " does not exist!")
-            
+        
         timeCost = time.time()
         updateTravelTime()
         print("Update travel time used: ", round(time.time() - timeCost, 2), "secs")
-        
+
         # All or nothing load
         SPTT, x_bar = loadAON()
         
@@ -375,17 +369,18 @@ def assignment(algorithm, accuracy = 0.01, maxIter=100):
         print("Modify attributs flow time used: ", round(time.time() - timeFlow, 2), "secs")
         
         # total demand assigned
-        TSTT = round(sum([a['flow'] * a['cost'] for a in g.es]), 3)
+        TSTT = round(sum([a['flow'] * a['cost_total'] for a in g.es]), 3)
         # total demand in reality
         SPTT = round(SPTT, 3)
         gap = round(abs((TSTT / SPTT) - 1), 5)
-        # print(TSTT, SPTT, gap)
+        print("Iteeration {} takes {} seconds, gap is {} with alpha {}.".format(it, round(time.time() - startI, 2) gap, alpha))
+        print(TSTT, SPTT, gap)
         it = it + 1
         if it > maxIter:
             print("The assignment did not converge with the desired gap and max iterations are reached")
             print("current gap ", gap)
             break
-    print("Assignment took", time.time() - startP, " seconds")
+    print("Assignment took", round(time.time() - startP, 2), " seconds")
     print("assignment converged in ", it-1, " iterations")
     print("current gap ", gap)
 
@@ -401,7 +396,7 @@ def writeUEresults(sep=';'):
     outFile.write(tmpOut+"\n")
     
     for edge in g.es:
-        tmpOut = str(g.vs[edge.target]['name']) + sep + str(g.vs[edge.source]['name']) + sep + str(edge['capacity']) + sep + str(edge['distance']) + sep + str(edge['cost']) + sep + str(edge['flow'])
+        tmpOut = str(g.vs[edge.target]['name']) + sep + str(g.vs[edge.source]['name']) + sep + str(edge['capacity']) + sep + str(edge['distance']) + sep + str(edge['cost_total']) + sep + str(edge['flow'])
         outFile.write(tmpOut + "\n")
     
     outFile.close()
@@ -435,7 +430,7 @@ if __name__ == '__main__':
     print("Reading the network data took", round(time.time() - readStart, 2), "secs")
     
     print("start assignement: ")
-    assignment("MSA", accuracy = 0.1, maxIter=3)
+    assignment("FW", accuracy = 0.01, maxIter=4)
 
 #%%
     writeUEresults()
